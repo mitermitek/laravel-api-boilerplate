1f2e1a8334ef31ddbcc84a8268c0a7e9124f1e6e
mitermitek <40229021+mitermitek@users.noreply.github.com>
2024-03-11 19:20:31 +0100

    Return JSON error response for exceptions for API requests

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,4 +27,13 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Throwable $e)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+
+        return parent::render($request, $e);
+    }
 }
